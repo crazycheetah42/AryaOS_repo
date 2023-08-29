f8f481c0c52f0c9453deff7d4c460e60a77be1e5
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-08-29 16:45:54 -0400

    Create gyanai_setup.sh

---
+++ gyanai_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo wget https://github.com/crazycheetah42/AryaOS_repo/releases/download/repo/gyanai -p /packages/aryaos/
+sudo wget https://raw.githubusercontent.com/crazycheetah42/GyanAI/main/key.txt -p /packages/aryaos/
+sudo wget https://raw.githubusercontent.com/crazycheetah42/GyanAI/main/openai.txt -p /packages/aryaos/
